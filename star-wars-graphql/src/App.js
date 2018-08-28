9af7085c71370db75fc1bae93a6147831cbe1849
Chris Palmer <chrispalmer191@gmail.com>
2018-08-28 13:30:15 -0600

    added material UI and worked with graphql

--- star-wars-graphql/src/App.js
+++ star-wars-graphql/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ApolloProvider } from "react-apollo";
 import './App.css';
+import ApolloClient from "apollo-boost";
+import Ships from "./AllStarShips";
+import SimpleAppBar from "./AppBar";
 
-class App extends Component {
+const client = new ApolloClient({
+  uri: "http://localhost:62695"
+});
+
+class AppTest extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+      <SimpleAppBar/>
+      <Ships/>
+      </ApolloProvider>
     );
   }
 }
 
-export default App;
+export default AppTest;
--- star-wars-graphql/src/index.js
+++ star-wars-graphql/src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppTest from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag"
-
-const client = new ApolloClient({
-  uri: "http://localhost:58535"
-});
-client
-  .query({
-    query: gql`
-      {
-        person(personID: 4) {
-          name
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
 
 
 ReactDOM.render(
-<App/>,
+<AppTest/>,
 document.getElementById('root'));
 registerServiceWorker();
